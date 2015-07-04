3b4850da81e566a8c2eda17bbd68408a23d86e1c
皆川彩 <minagawaaya@minakawaaya-no-MacBook-Air.local>
2015-07-04 13:44:18 +0900

    ヘッダーのモバイル版も大体できた！

--- app/assets/javascripts/project/dropdown.js
+++ app/assets/javascripts/project/dropdown.js
@@ -1,4 +1,15 @@
-$('#category-dropdown-switch').click(function(){
-  $('#category-dropdown-box').toggle();
-  return false;
+$(function(){
+  //トップページのカテゴリー表示トグルボタン
+  $('#category-dropdown-switch').click(function(){
+    $('#category-dropdown-box').toggle();
+    return false;
+  });
+
+  //サイドバー表示ボタン
+  $("body").mobile_menu({
+    menu: ['#main-nav #slidemenu_contents' ], //オフキャンバスに含めるobj
+    menu_width: 250,  //メニューの横幅
+    prepend_button_to: '#menu-btn',  //トリガーになるobjを指定
+    button_content:'<i class="icon-bars"></i><br>メニュー' 
+  });
 });
