9407a8b19944dcd747b4168cfb3218fba8b2b369
皆川彩 <minagawaaya@minakawaaya-no-MacBook-Air.local>
2015-07-06 11:58:11 +0900

    footerできた！

--- app/assets/javascripts/project/dropdown.js
+++ app/assets/javascripts/project/dropdown.js
@@ -9,7 +9,7 @@ $(function(){
   $("body").mobile_menu({
     menu: ['#main-nav #slidemenu_contents' ], //オフキャンバスに含めるobj
     menu_width: 250,  //メニューの横幅
-    prepend_button_to: '#menu-btn',  //トリガーになるobjを指定
-    button_content:'<i class="icon-bars"></i><br>メニュー' 
+    prepend_button_to: '.sp-header-top__menu__sidebar'  //トリガーになるobjを指定
   });
+
 });
