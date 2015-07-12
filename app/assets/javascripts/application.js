c109dc4533024bfb57ddebb325db774e4f86f330
皆川彩 <minagawaaya@minakawaaya-no-MacBook-Air.local>
2015-07-12 17:31:51 +0900

    レシピnewおおまか完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,52 @@
 //= require_directory ./project
 //= require jquery_ujs
 //= require_tree .
+
+//プレビュー画像を表示
+$(function(){
+  var setFileInput = $('.imgInput'),
+  setFileImg = $('.imgView');
+  
+  setFileInput.each(function(){
+    var selfFile = $(this),
+    selfInput = $(this).find('input[type=file]'),
+    prevElm = selfFile.find(setFileImg),
+    orgPass = prevElm.attr('src');
+    
+    selfInput.change(function(){
+      var file = $(this).prop('files')[0],
+      fileRdr = new FileReader();
+      
+      if (!this.files.length){
+        prevElm.attr('src', orgPass);
+        return;
+      } else {
+        if (!file.type.match('image.*')){
+          prevElm.attr('src', orgPass);
+          return;
+        } else {
+          fileRdr.onload = function() {
+            prevElm.attr('src', fileRdr.result);
+          }
+          fileRdr.readAsDataURL(file);
+        }
+      }
+    });
+  });
+});
+
+
+//材料、作り方のフォームの追加、削除
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").remove();
+}
+
+function add_fields(link, association, content) {
+  $fields = $("." + association).find(".fields")
+  // console.log($fields.filter(":last").find(".step-count").text());
+  var new_id = $fields[0] ? $fields.filter(":last").find(".step-count").text() : new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
+  // $(link).parents("." + association).find(".step-count").text(new_id + 1);
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+
+  def link_to_add_fields(name, f, association, options = {})
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render("fields_" + association.to_s.singularize, :f => builder)
+    end
+    link_to_function(name, raw("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"), options)
+  end
+
+  #current_userの存在の確認
+  def current_user?(user)
+    user == current_user
+  end
+
+  def current_nutritionist?(nutritionist)
+    nutritionist == current_nutritionist
+  end
+
+  # 改行文字を<br>に変換する
+  def br(str)
+    html_escape(str).gsub(/\r\n|\r|\n/, "<br />").html_safe
+  end
 end
