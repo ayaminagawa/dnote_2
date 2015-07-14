b7dc4e403db569013a0767f85011b6079bd9d72a
皆川彩 <minagawaaya@minakawaaya-no-MacBook-Air.local>
2015-07-14 17:30:01 +0900

    レシピ、メニュー一覧ページ完成

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,12 +2,13 @@ class AboutController < ApplicationController
   def index
   	# @favorite_recipes = Favorite.favorite_count.map{|recipe_id| Recipe.find_by(id: recipe_id)}
     @favorite_recipes = Recipe.limit(3)
-  	@todays_menu = Menu.find(:last)
+  	# @todays_menu = Menu.find(:last)
+    @todays_menu = Menu.last
 
   	@todays_recipes = @todays_menu.recipes
     @main_recipe = @todays_recipes.select{|recipe| recipe.recipe_select == 1}.first
-    @side_recipes = @todays_recipes.select{|recipe| recipe.recipe_select == 2}
-    @side_recipes = @side_recipes.compact
+    @side_recipes = @todays_recipes.select{|recipe| recipe.recipe_select == 2}.compact
+    # @side_recipes = @side_recipes
     # @count = @todays_recipes.count
     # @main_recipe = @main_recipes.first
     # @side1_recipe = @side1_recipes.first
